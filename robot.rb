5f5bda46f04dd5d182403bdd431f4c6454061b34
spencermcw <spencer.mcw@gmail.com>
2015-12-01 13:39:16 -0800

    pretty much done

--- robot.rb
+++ robot.rb
@@ -10,7 +10,7 @@ def get_observations
     if observation.include? "S" then val += 4 end
     if observation.include? "W" then val += 2 end
     if observation.include? "E" then val += 1 end
-    ret_array.push(val)
+    ret_array.push(("%04b" % val))
   end
   return ret_array
 end
@@ -36,18 +36,13 @@ def get_error
   return Array.new(5){|index| (e**index * (1-e)**(4-index)).round(6)}
 end
 
-def get_initial_transitivity_for(_world)
-  # determine the width of the world
-  world_width = _world.column_vectors().size
-  world_height = _world.row_vectors().size
-  world_size = world_width*world_height
+def get_initial_trans_for(_world)
   # create new trans matrix
-  trans_array = Array.new(world_size).map!{Array.new(world_size,0)}
-  print trans_array, "\n"
+  trans_array = Array.new(_world.size).map!{Array.new(_world.size,0)}
   # iterate over each square in the world and update trans matrix appropriately
-  _world.each_with_index do |e, row, col|
+  _world.rooms.each_with_index do |e, row, col|
     # cell represents the current cell we are analyzing in the world
-    cell = row*world_width+col
+    cell = row*_world.width+col
     # prob is default of 0
     prob = 0
     # determine probability of moving to adjacent squares
@@ -56,8 +51,8 @@ def get_initial_transitivity_for(_world)
     # nav is now holding an array to access each direction by index
     e = e.to_s.split("").map(&:to_i)
     # initialize t matrix for given possible navigation directions
-    if e[0]==0 then trans_array[cell-world_width][cell] = prob end
-    if e[1]==0 then trans_array[cell+world_width][cell] = prob end
+    if e[0]==0 then trans_array[cell-_world.width][cell] = prob end
+    if e[1]==0 then trans_array[cell+_world.width][cell] = prob end
     if e[2]==0 then trans_array[cell-1][cell] = prob end
     if e[3]==0 then trans_array[cell+1][cell] = prob end
   end
@@ -67,39 +62,98 @@ end
 
 def get_initial_joint_prediction_matrix_for(_world)
   # determine the width of the world
-  world_width = _world.column_vectors().size
-  world_height = _world.row_vectors().size
-  world_size = world_width*world_height
+  # world_width = _world.column_vectors().size
+  # world_height = _world.row_vectors().size
+  # world_size = world_width*world_height
   # initialize prediction matrix array
-  pred_array = Array.new(world_size).map {Array.new(1,0)}
+  pred_array = Array.new(_world.size).map {Array.new(1,0)}
   # initialize probability that will be assigned to all eligible rooms
-  prob = 1.0/(world_size-_world.each.count("1111"))
+  prob = 1.0/(_world.size-_world.rooms.each.count("1111"))
   # for each room in the world see if it's eligible and set it
-  _world.each_with_index do |e, row, col|
-    unless e == "1111" then pred_array[(row*world_width)+col] = [prob] end
+  _world.rooms.each_with_index do |e, row, col|
+    unless e == "1111" then pred_array[(row*_world.width)+col] = [prob] end
   end
   # turn it into a Matrix! fckyea
   return Matrix.rows(pred_array)
 end
 
+def get_initial_observation_probability_matrix(world, observation)
 
+end
 
+# class to hold common properties of a world
+class World
+  # rooms contain binary representation of unobscured directions of movement
+  def rooms
+    @rooms
+  end
+  def size
+    @size
+  end
+  def width
+    @width
+  end
+  def height
+    @height
+  end
+  def initialize(_world)
+    @rooms = _world
+    @width = @rooms.column_count()
+    @height = @rooms.row_count()
+    @size = @width*@height
+  end
+end
 
+def main
+  # read observations from input file
+  observations = get_observations
+  # generate a new world given the input file
+  world = World.new(get_world)
+  # generate error table
+  error = get_error
+  # initialize transitivity matrix for problem given a world
+  trans_matrix = get_initial_trans_for(world)
+  # initialize joint prediction matrix given a world
+  joint_prediction_matrix = get_initial_joint_prediction_matrix_for(world)
 
-observations = get_observations
-world = get_world
-error = get_error
-transitivity_matrix = get_initial_transitivity_for(world)
-joint_prediction_matrix = get_initial_joint_prediction_matrix_for(world)
+  # initialize joint transitivity matrix for first iteration
+  joint_trans_matrix = trans_matrix*joint_prediction_matrix
+  # Initialize Observation Probability Matrix
+  obs_prob_matrix = Matrix.build(world.size, world.size) {0}
+  # Initialize Y
+  y = Matrix.zero(0)
+  # update matricies for each observation
+  observations.each do |observation|
+    # print current iteration for debugging
+    puts "observation: #{observation}\n"
+    # for each room: update the observation probability matrix
+    world.rooms.each_with_index do |room, row, col|
+      # must copy matrix into an array in order to change single components
+      tmp_array = obs_prob_matrix.to_a
+      # adjust the array accordingly to the xor between observation and rooms
+      tmp_array[(row*world.width)+col][(row*world.width)+col] = error[("%04b" % (room.to_i(2)^observation.to_i(2))).count("1")]
+      # rebuild the matrix using the new values
+      obs_prob_matrix = Matrix.rows(tmp_array)
+    end
+    # update Y
+    y = obs_prob_matrix*joint_trans_matrix
+    # update joint transitivity matrix
+    joint_trans_matrix = trans_matrix*y
+  end
+  # after final iteration, calculate most likely room
+  # sum components of y and calculate E
+  sum = 0
+  y.each {|i| sum += i }
+  e = (1.0/sum) * y
+  e = e.column(0).to_a
+  # find the highest probability in E
+  max = e.max
+  # initialize an array
+  a = Array.new()
+  # push the room index of the most likely rooms
+  e.each_with_index {|e, i| if e == max then a.push(i) end}
+  # debugging
+  puts a
+end
 
-# need to create iteration
-
-
-puts
-puts
-puts "LOGGING..."
-print "observations:\t#{observations}\n"
-print "world:\t\t#{world}\n"
-print "error:\t\t#{error}\n"
-print "t:\t\t#{transitivity_matrix}\n"
-print "joint p:\t#{joint_prediction_matrix}\n"
+main
